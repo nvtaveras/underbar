e75d7bc06457b1f37d0b0a7f8dec83730c1484e6
Omar Duarte <omarduarte@gmail.com>
2014-10-16 16:22:47 -0400

    Completed _.some. Couldn't re-use _.every as my implementation of it probably doesn't work along with _.some.

--- src/underbar.js
+++ src/underbar.js
@@ -224,6 +224,14 @@ var _ = {};
   // provided, provide a default one
   _.some = function(collection, iterator) {
     // TIP: There's a very clever way to re-use every() here.
+
+    if (typeof iterator === "undefined") {
+      iterator = _.identity;
+    }
+
+    return _.reduce(collection, function(isTrue, current) {
+      return !!(iterator(current)) || isTrue;
+    }, false)
   };
 
 
